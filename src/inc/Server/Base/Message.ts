37bccb8d9ba2d35b7180949965b904ff2fd8a284
Stefan Werfling <stefan.werfling@pegenau.de>
2024-04-17 11:12:14 +0200

    update message

--- src/inc/Server/Base/Message.ts
+++ src/inc/Server/Base/Message.ts
@@ -11,10 +11,36 @@ export class Message {
 
     /**
      * Create a message instance
-     * @param {string|Buffer} data
+     * @param {string|Buffer|Message} data
      */
-    public constructor(data: string|Buffer) {
-        this._data = data;
+    public constructor(data: string|Buffer|Message) {
+        if (data instanceof Message) {
+            this._data = data.getBuffer();
+        } else {
+            this._data = data;
+        }
+    }
+
+    /**
+     * Return a Buffer
+     */
+    public getBuffer(): Buffer {
+        if (typeof this._data === 'string') {
+            return Buffer.from(this._data, 'utf-8');
+        }
+
+        return this._data;
+    }
+
+    /**
+     * Return the size of Message
+     */
+    public getSize(): number {
+        if (typeof this._data === 'string') {
+            return this._data.length;
+        }
+
+        return this._data.length;
     }
 
     /**
